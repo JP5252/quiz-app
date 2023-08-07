e683c1854b19c99790ca81bfc10b3d4f4082e10b
JP5252 <justin.pearson52@hotmail.com>
2023-08-06 19:31:40 -0600

    move questions to API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
 import Quiz from "./Quiz";
-import { jsQuizz } from "./constants";
 
 function App() {
-	return <Quiz questions={jsQuizz.questions}/>;
+	const [questions, setQuestions] = useState([]);
+	useEffect(() => {
+		getQuestions();
+	}, []);
+
+	const getQuestions = async () => {
+		try {
+			const response = await fetch(
+				"https://64d0405bff953154bb78b8ef.mockapi.io/questions"
+			);
+			const questionsResponse = await response.json();
+			setQuestions(questionsResponse);
+		} catch(error) {
+			console.log(error);
+		}
+	};
+
+	return (questions.length && <Quiz questions={questions}/>);
 }
 
 export default App
